829eaa71064202a85b1534eaf29ae4b5dbc4985e
jerahmeel-hipolito <hipolitojerahmeel12@gmail.com>
2026-07-08 18:20:25 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -21,7 +21,7 @@
 const Api = {
   // Replace with the deployed Apps Script web app URL after deployment.
   // See apps-script/CodeJS.gs deployment notes.
-  ENDPOINT_URL: 'PASTE_DEPLOYED_APPS_SCRIPT_URL_HERE',
+  ENDPOINT_URL: 'https://script.google.com/macros/s/AKfycbw49J1j7W7SWviyWtGCZ4tfnzrBM6XKXK8kSNTD8oNtJf3e-WEb4ZyV-H891gSnUAEvQw/exec',
 
   async send(payload) {
     try {
